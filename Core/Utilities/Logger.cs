3aa04062fb4e40f912b64cce3969f34e79d65726
Alex <alexzoetermeer@gmail.com>
2019-08-11 10:46:41 +0200

    added simple logger class

---
+++ Core/Utilities/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Core.Utilities
+{
+    public class Logger
+    {
+        private readonly string prefix;
+
+        public Logger(string prefix = "")
+        {
+            this.prefix = prefix;
+        }
+
+        public void Log(string line, string prefix = null)
+        {
+            Console.WriteLine($"{DateTime.UtcNow} [{prefix ?? this.prefix}] {line}");
+        }
+    }
+}

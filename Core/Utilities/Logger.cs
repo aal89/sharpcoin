f639ba39d9cd27b451cbf5435d75c1af3c9c5973
Alex <alexzoetermeer@gmail.com>
2019-08-11 11:29:58 +0200

    added extra log options

--- Core/Utilities/Logger.cs
+++ Core/Utilities/Logger.cs
@@ -11,7 +11,17 @@ namespace Core.Utilities
             this.prefix = prefix;
         }
 
-        public void Log(string line, string prefix = null)
+        public void Append(string line)
+        {
+            Console.WriteLine($"{line}");
+        }
+
+        public void Line(string line, string prefix = null)
+        {
+            Console.Write($"{DateTime.UtcNow} [{prefix ?? this.prefix}] {line}");
+        }
+
+        public void NewLine(string line, string prefix = null)
         {
             Console.WriteLine($"{DateTime.UtcNow} [{prefix ?? this.prefix}] {line}");
         }

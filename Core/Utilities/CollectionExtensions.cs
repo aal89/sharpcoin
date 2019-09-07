fb3303ad511eaec9b4cc3174c707fe93ca783cba
Alex <alexzoetermeer@gmail.com>
2019-09-07 14:11:53 +0200

    added additional collection extensions

--- Core/Utilities/CollectionExtensions.cs
+++ Core/Utilities/CollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Core.Utilities
 {
@@ -10,7 +11,22 @@ namespace Core.Utilities
             return self.Map(obj => obj.ToString()).Reduce(R.Concat(delimiter), "");
         }
 
+        public static bool ContainsDuplicates<T>(this T[] self) where T: IEquatable<T>
+        {
+            return self.Distinct().Count() != self.Count();
+        }
+
         public static string Stringified<T>(this List<T> self, string delimiter = null)
+        {
+            return self.ToArray().Stringified(delimiter);
+        }
+
+        public static bool ContainsDuplicates<T>(this IEnumerable<T> self) where T : IEquatable<T>
+        {
+            return self.ToArray().ContainsDuplicates();
+        }
+
+        public static string Stringified<T>(this HashSet<T> self, string delimiter = null)
         {
             return self.ToArray().Stringified(delimiter);
         }

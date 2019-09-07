b0fb8d6101d5ae72a6b4ead60a98fd47d998c702
Alex <alexzoetermeer@gmail.com>
2019-09-07 14:12:51 +0200

    made the controlflag pass by reference

--- Core/Miner/Miner.cs
+++ Core/Miner/Miner.cs
@@ -5,7 +5,7 @@ namespace Core
 {
     public static class Miner
     {
-        public static Block Solve(Block Block, ulong TargetDiff, bool ControlFlag = true)
+        public static Block Solve(Block Block, ulong TargetDiff, in bool ControlFlag = true)
         {
             while (Block.GetDifficulty() > TargetDiff && ControlFlag)
             {
@@ -17,7 +17,7 @@ namespace Core
             return Block.GetDifficulty() < TargetDiff ? Block : null;
         }
 
-        public static Block Solve(SharpKeyPair skp, Blockchain bc, bool ControlFlag = true)
+        public static Block Solve(SharpKeyPair skp, Blockchain bc, in bool ControlFlag = true)
         {
             return Solve(Block.Create(skp, bc), bc.GetDifficulty(), ControlFlag);
         }

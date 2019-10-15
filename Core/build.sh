1d2824178ce09f090a69e2d05e99bb5e13433101
Alex <alexzoetermeer@gmail.com>
2019-10-15 12:07:36 +0200

    updated build script

--- Core/build.sh
+++ Core/build.sh
@@ -5,13 +5,29 @@ dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFil
 dotnet restore
 
 cd bin/Release/netcoreapp3.0/win-x86
-zip -r sharpcoin-core-v0.1_win-x86.zip publish
+rm -r sharpcoin-core-v0.1_win-x86
+rm sharpcoin-core-v0.1_win-x86.zip
+
+mv publish sharpcoin-core-v0.1_win-x86
+zip -r sharpcoin-core-v0.1_win-x86.zip sharpcoin-core-v0.1_win-x86
 
 cd ../win-x64
-zip -r sharpcoin-core-v0.1_win-x64.zip publish
+rm -r sharpcoin-core-v0.1_win-x64
+rm sharpcoin-core-v0.1_win-x64.zip
+
+mv publish sharpcoin-core-v0.1_win-x64
+zip -r sharpcoin-core-v0.1_win-x64.zip sharpcoin-core-v0.1_win-x64
 
 cd ../linux-x64
-zip -r sharpcoin-core-v0.1_linux-x64.zip publish
+rm -r sharpcoin-core-v0.1_linux-x64
+rm sharpcoin-core-v0.1_linux-x64.zip
+
+mv publish sharpcoin-core-v0.1_linux-x64
+zip -r sharpcoin-core-v0.1_linux-x64.zip sharpcoin-core-v0.1_linux-x64
 
 cd ../osx-x64
-zip -r sharpcoin-core-v0.1_osx-x64.zip publish
+rm -r sharpcoin-core-v0.1_osx-x64
+rm sharpcoin-core-v0.1_osx-x64.zip
+
+mv publish sharpcoin-core-v0.1_osx-x64
+zip -r sharpcoin-core-v0.1_osx-x64.zip sharpcoin-core-v0.1_osx-x64

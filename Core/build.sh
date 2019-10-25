717c9f914df4412f2b7119a19684632ddcded217
Alex <alexzoetermeer@gmail.com>
2019-10-25 22:40:34 +0200

    removed linux as target platform

--- Core/build.sh
+++ Core/build.sh
@@ -1,7 +1,6 @@
 dotnet publish -c Release -r win-x86 --self-contained true /p:PublishSingleFile=true
 dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true
 dotnet publish -c Release -r osx-x64 --self-contained true /p:PublishSingleFile=true
-dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true
 dotnet restore
 
 cd bin/Release/netcoreapp3.0/win-x86
@@ -17,13 +16,6 @@ rm sharpcoin-core-v0.1_win-x64.zip
 
 mv publish sharpcoin-core-v0.1_win-x64
 zip -r sharpcoin-core-v0.1_win-x64.zip sharpcoin-core-v0.1_win-x64
-
-cd ../linux-x64
-rm -r sharpcoin-core-v0.1_linux-x64
-rm sharpcoin-core-v0.1_linux-x64.zip
-
-mv publish sharpcoin-core-v0.1_linux-x64
-zip -r sharpcoin-core-v0.1_linux-x64.zip sharpcoin-core-v0.1_linux-x64
 
 cd ../osx-x64
 rm -r sharpcoin-core-v0.1_osx-x64

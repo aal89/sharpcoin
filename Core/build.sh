b34b0f9267a8255b882dcd85c099efaae20c9ec7
Alex <alexzoetermeer@gmail.com>
2019-10-27 14:26:30 +0100

    generalized the build script and updated version number to 0.1.1

--- Core/build.sh
+++ Core/build.sh
@@ -1,25 +1,27 @@
+VERSION=`cat Core.sln | grep version | tail -c 7 | rev | cut -c 2- | rev`
+
 dotnet publish -c Release -r win-x86 --self-contained true /p:PublishSingleFile=true
 dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true
 dotnet publish -c Release -r osx-x64 --self-contained true /p:PublishSingleFile=true
 dotnet restore
 
 cd bin/Release/netcoreapp3.0/win-x86
-rm -r sharpcoin-core-v0.1_win-x86
-rm sharpcoin-core-v0.1_win-x86.zip
+rm -r sharpcoin-core-v$VERSION_win-x86
+rm sharpcoin-core-v$VERSION_win-x86.zip
 
-mv publish sharpcoin-core-v0.1_win-x86
-zip -r sharpcoin-core-v0.1_win-x86.zip sharpcoin-core-v0.1_win-x86
+mv publish sharpcoin-core-v$VERSION_win-x86
+zip -r sharpcoin-core-v$VERSION_win-x86.zip sharpcoin-core-v$VERSION_win-x86
 
 cd ../win-x64
-rm -r sharpcoin-core-v0.1_win-x64
-rm sharpcoin-core-v0.1_win-x64.zip
+rm -r sharpcoin-core-v$VERSION_win-x64
+rm sharpcoin-core-v$VERSION_win-x64.zip
 
-mv publish sharpcoin-core-v0.1_win-x64
-zip -r sharpcoin-core-v0.1_win-x64.zip sharpcoin-core-v0.1_win-x64
+mv publish sharpcoin-core-v$VERSION_win-x64
+zip -r sharpcoin-core-v$VERSION_win-x64.zip sharpcoin-core-v$VERSION_win-x64
 
 cd ../osx-x64
-rm -r sharpcoin-core-v0.1_osx-x64
-rm sharpcoin-core-v0.1_osx-x64.zip
+rm -r sharpcoin-core-v$VERSION_osx-x64
+rm sharpcoin-core-v$VERSION_osx-x64.zip
 
-mv publish sharpcoin-core-v0.1_osx-x64
-zip -r sharpcoin-core-v0.1_osx-x64.zip sharpcoin-core-v0.1_osx-x64
+mv publish sharpcoin-core-v$VERSION_osx-x64
+zip -r sharpcoin-core-v$VERSION_osx-x64.zip sharpcoin-core-v$VERSION_osx-x64
